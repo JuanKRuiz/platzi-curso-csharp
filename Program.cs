10e032a2fbb2bbbdf9010c9d328f3cec88afa59e
JuanKRuiz <juank@outlook.com>
2018-09-13 22:58:53 -0500

    ETapa9

--- Program.cs
+++ Program.cs
@@ -25,6 +25,59 @@ namespace CoreEscuela
             var listaEvalXAsig = reporteador.GetDicEvaluaXAsig();
             var listaPromXAsig = reporteador.GetPromeAlumnPorAsignatura();
 
+            Printer.WriteTitle("Captura de una Evaluación por Consola");
+            var newEval = new Evaluación();
+            string nombre, notastring;
+            float nota;
+
+            WriteLine("Ingrese el nombre de la evaluación");
+            Printer.PresioneENTER();
+            nombre = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                Printer.WriteTitle("El valor del nombre no puede ser vacio");
+                WriteLine("Saliendo del programa");
+            }
+            else
+            {
+                newEval.Nombre = nombre.ToLower();
+                WriteLine("El nombre de la evaluacion ha sido ingresado correctamente");
+            }
+
+
+            WriteLine("Ingrese la nota de la evaluación");
+            Printer.PresioneENTER();
+            notastring = Console.ReadLine();
+
+
+            try
+            {
+                newEval.Nota = float.Parse(notastring);
+                if (newEval.Nota < 0 || newEval.Nota > 5)
+                {
+                    throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
+                }
+                WriteLine("La nota de la evaluacion ha sido ingresada correctamente");
+                return;
+            }
+            catch (ArgumentOutOfRangeException arge)
+            {
+                Printer.WriteTitle(arge.Message);
+                WriteLine("Saliendo del programa");
+            }
+            finally
+            {
+                Printer.WriteTitle("FINALLY");
+                Printer.Beep(2500, 500, 3);
+
+            }
+            // catch(Exception)
+            // {
+            //     Printer.WriteTitle("El valor de la nota no es un número válido");
+            //     WriteLine("Saliendo del programa");
+
+
         }
 
         private static void AccionDelEvento(object sender, EventArgs e)
--- Util/Printer.cs
+++ Util/Printer.cs
@@ -9,6 +9,10 @@ namespace CoreEscuela.Util
             WriteLine("".PadLeft(tam, '='));
         }
 
+        public static void PresioneENTER()
+        {
+            WriteLine("Presione ENTER para continuar");
+        }
         public static void WriteTitle(string titulo)
         {
             var tamaño =titulo.Length + 4;
